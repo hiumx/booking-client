7c7188529e5462d971e4980929ffc8cfd812e39e
hiumx <maixuanhieu250103@gmail.com>
2024-05-14 22:27:37 +0700

    [FIXBUG] Fix(edit) classname of AuthForm and Input component

--- src/components/AuthForm/index.js
+++ src/components/AuthForm/index.js
@@ -8,26 +8,26 @@ const AuthForm = ({ title, action, children }) => {
     return (
         <div>
             <AuthLayout>
-                <div className='login__content'>
+                <div className='auth__content'>
                     <h6>{title}</h6>
                     <div>
                         {children}
-                        <button className='login__btn'>{action}</button>
-                        <div className='login__title__others__wrapper'>
-                            <hr className='login__title__others__hr' />
-                            <span className='login__title__others'>or use one of these options</span>
-                            <hr className='login__title__others__hr' />
+                        <button className='auth__btn'>{action}</button>
+                        <div className='auth__title__others__wrapper'>
+                            <hr className='auth__title__others__hr' />
+                            <span className='auth__title__others'>or use one of these options</span>
+                            <hr className='auth__title__others__hr' />
                         </div>
-                        <ul className='login__list__social__media'>
-                            <li className='login__item__sm'>
+                        <ul className='auth__list__social__media'>
+                            <li className='auth__item__sm'>
                                 <FacebookIcon className='facebook__icon' width='34px' height='34px' fill='#3a64c4' />
                             </li>
-                            <li className='login__item__sm'>
+                            <li className='auth__item__sm'>
                                 <GoogleIcon className='google__icon' width='32px' height='32px' fill='#b70100' />
                             </li>
                         </ul>
                         <hr />
-                        <div className='login__policy'>
+                        <div className='auth__policy'>
                             <p>By signing in or creating an account, you agree with our <a href='#'>Terms & conditions</a> and <a href='#'>Privacy statement</a></p>
                             <p>All rights reserved.</p>
                             <p>Copyright (2006 - 2024) - Booking™</p>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import './_login.scss';
 import AuthForm from '~/components/AuthForm';
-import Button from '~/components/Button';
+import Input from '~/components/Input';
 
 const Login = () => {
   return (
 
     <div className='login__wrapper'>
       <AuthForm title="Sign in" action="Sign in">
-        <Button
+        <Input
           type="email"
           htmlFor="email"
           labelName="Email address or phone number"
           placeholder="Enter your email address"
         />
-        <Button
+        <Input
           type="password"
           htmlFor="password"
           labelName="Password"
           placeholder="Enter your password"
         />
+        <a className='login__forgot__password' href='#'>Forgot password?</a>
       </AuthForm>
     </div>
   )
