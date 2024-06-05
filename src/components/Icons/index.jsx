a46c2da18717d65c4acd47c9234399ad731357bb
Mai Xuân Hiếu <maixuanhieu250103@gmail.com>
2024-06-05 23:28:14 +0700

    [FEATURE] UI for Home page

--- src/components/Icons/index.jsx
+++ src/components/Icons/index.jsx
@@ -214,6 +214,19 @@ export const CheckIcon = ({ width = "16px", height = "16px", className = "", fil
     )
 }
 
+export const ChevronLeftIcon = ({ width = "16px", height = "16px", className = "", fill = "" }) => {
+    return (
+        <svg
+            width={width}
+            height={height}
+            className={className}
+            fill={fill}
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+            <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+        </svg>
+    )
+}
+
 export const ChevronRightIcon = ({ width = "16px", height = "16px", className = "", fill = "" }) => {
     return (
         <svg
@@ -444,6 +457,45 @@ export const BreakfastIcon = ({ width = "16px", height = "16px", className = "",
             fill={fill}
             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
             <path d="M416 0C400 0 288 32 288 176V288c0 35.3 28.7 64 64 64h32V480c0 17.7 14.3 32 32 32s32-14.3 32-32V352 240 32c0-17.7-14.3-32-32-32zM64 16C64 7.8 57.9 1 49.7 .1S34.2 4.6 32.4 12.5L2.1 148.8C.7 155.1 0 161.5 0 167.9c0 45.9 35.1 83.6 80 87.7V480c0 17.7 14.3 32 32 32s32-14.3 32-32V255.6c44.9-4.1 80-41.8 80-87.7c0-6.4-.7-12.8-2.1-19.1L191.6 12.5c-1.8-8-9.3-13.3-17.4-12.4S160 7.8 160 16V150.2c0 5.4-4.4 9.8-9.8 9.8c-5.1 0-9.3-3.9-9.8-9L127.9 14.6C127.2 6.3 120.3 0 112 0s-15.2 6.3-15.9 14.6L83.7 151c-.5 5.1-4.7 9-9.8 9c-5.4 0-9.8-4.4-9.8-9.8V16zm48.3 152l-.3 0-.3 0 .3-.7 .3 .7z" />
+        </svg>
+    )
+}
+
+export const ClockRotateLeftIcon = ({ width = "16px", height = "16px", className = "", fill = "" }) => {
+    return (
+        <svg
+            width={width}
+            height={height}
+            className={className}
+            fill={fill}
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path d="M75 75L41 41C25.9 25.9 0 36.6 0 57.9V168c0 13.3 10.7 24 24 24H134.1c21.4 0 32.1-25.9 17-41l-30.8-30.8C155 85.5 203 64 256 64c106 0 192 86 192 192s-86 192-192 192c-40.8 0-78.6-12.7-109.7-34.4c-14.5-10.1-34.4-6.6-44.6 7.9s-6.6 34.4 7.9 44.6C151.2 495 201.7 512 256 512c141.4 0 256-114.6 256-256S397.4 0 256 0C185.3 0 121.3 28.7 75 75zm181 53c-13.3 0-24 10.7-24 24V256c0 6.4 2.5 12.5 7 17l72 72c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-65-65V152c0-13.3-10.7-24-24-24z" />
+        </svg>
+    )
+}
+
+export const CartFlatbedIcon = ({ width = "16px", height = "16px", className = "", fill = "" }) => {
+    return (
+        <svg
+            width={width}
+            height={height}
+            className={className}
+            fill={fill}
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+            <path d="M0 32C0 14.3 14.3 0 32 0H48c44.2 0 80 35.8 80 80V368c0 8.8 7.2 16 16 16H608c17.7 0 32 14.3 32 32s-14.3 32-32 32H541.3c1.8 5 2.7 10.4 2.7 16c0 26.5-21.5 48-48 48s-48-21.5-48-48c0-5.6 1-11 2.7-16H253.3c1.8 5 2.7 10.4 2.7 16c0 26.5-21.5 48-48 48s-48-21.5-48-48c0-5.6 1-11 2.7-16H144c-44.2 0-80-35.8-80-80V80c0-8.8-7.2-16-16-16H32C14.3 64 0 49.7 0 32zM432 96V56c0-4.4-3.6-8-8-8H344c-4.4 0-8 3.6-8 8V96h96zM288 96V56c0-30.9 25.1-56 56-56h80c30.9 0 56 25.1 56 56V96 320H288V96zM512 320V96h16c26.5 0 48 21.5 48 48V272c0 26.5-21.5 48-48 48H512zM240 96h16V320H240c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48z" />
+        </svg>
+    )
+}
+
+export const TagIcon = ({ width = "16px", height = "16px", className = "", fill = "" }) => {
+    return (
+        <svg
+            width={width}
+            height={height}
+            className={className}
+            fill={fill}
+            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M0 80V229.5c0 17 6.7 33.3 18.7 45.3l176 176c25 25 65.5 25 90.5 0L418.7 317.3c25-25 25-65.5 0-90.5l-176-176c-12-12-28.3-18.7-45.3-18.7H48C21.5 32 0 53.5 0 80zm112 32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
         </svg>
     )
 }
