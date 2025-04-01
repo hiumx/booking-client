9fff1573cfe01e7df56c2652e1d03b5ee680c952
Mai Xuân Hiếu <maixuanhieu250103@gmail.com>
2025-04-01 11:23:19 +0700

    [FEAT] Format source code

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,36 +2,36 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useFetch = (url) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const res = await axios.get(url);
-        setData(res.data);
-      } catch (err) {
-        setError(err);
-      }
-      setLoading(false);
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const res = await axios.get(url);
+                setData(res.data);
+            } catch (err) {
+                setError(err);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [url]);
+
+    const reFetch = async () => {
+        setLoading(true);
+        try {
+            const res = await axios.get(url);
+            setData(res.data);
+        } catch (err) {
+            setError(err);
+        }
+        setLoading(false);
     };
-    fetchData();
-  }, [url]);
 
-  const reFetch = async () => {
-    setLoading(true);
-    try {
-      const res = await axios.get(url);
-      setData(res.data);
-    } catch (err) {
-      setError(err);
-    }
-    setLoading(false);
-  };
-
-  return { data, loading, error, reFetch };
+    return { data, loading, error, reFetch };
 };
 
 export default useFetch;
